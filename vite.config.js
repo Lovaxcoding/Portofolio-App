a6958e74d976a25a0d006a9ab8705db22a224620
LovaxCoding <lnantenaina78@gmail.com>
2025-02-06 23:35:28 +0100

    Rollup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portofolio-App/',
   build: {
     outDir: 'build',  // Dossier de sortie où seront générés les fichiers
     sourcemap: true,  // Génère un fichier de source map pour le débogage si nécessaire
