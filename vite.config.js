d92fc11d093fb2d48bbc9c1cae8165fc93b8db71
LovaxCoding <lnantenaina78@gmail.com>
2025-02-06 23:13:40 +0100

    Build js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',  // Dossier de sortie où seront générés les fichiers
+    sourcemap: true,  // Génère un fichier de source map pour le débogage si nécessaire
+    rollupOptions: {
+      input: 'index.html',  // Assure-toi que le point d'entrée est bien index.html
+    }
+  }
+});
